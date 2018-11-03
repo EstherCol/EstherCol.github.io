971eef70dabe89437a82499458b80778f8a656c3
con-error <41839829+EstherCol@users.noreply.github.com>
2018-11-03 16:31:47 +0300

    fortuna

--- js/main.js
+++ js/main.js
@@ -1,70 +1,51 @@
-new WOW().init();
-document.addEventListener('scroll', function() {
-	if (document.documentElement.scrollTop + window.innerHeight >= document.documentElement.offsetHeight)
-		alert('Конец страницы');
+$(document).ready(function(){
+   $(".owl-carousel").owlCarousel({
+      items:1,
+      loop:true,
+      dots: true
+   });
+
+
+   // sticky header for <= tablets
+   // $( window ).scroll(function() {
+   //    let query = window.matchMedia('(max-width: 768px)');
+   //
+   //    if (query.matches) {
+   //       const header = $('.header');
+   //
+   //       if($(window).scrollTop()) {
+  	// 			header.addClass('fixed');
+   //          $('.nav-category').css({'marginTop' : '129.5px'});
+  	// 		}else {
+  	// 			header.removeClass('fixed');
+   //          $('.nav-category').css({'marginTop' : '0'});
+  	// 		}
+   //    }
+   // });
+
+
+   // header gamburger toggle
+   $('.header__box-gamburger').click(function () {
+      $(this).toggleClass('header__box-gamburger--active');
+      $('.sidebar').toggle();
+   });
+
+   // main info toggle
+   $('.info__btn-show-all').click(function () {
+      const content = $('.info__toggle');
+      content.css('height', '100%');
+      $('.info__fade-out').remove();
+      $(this).remove();
+   })
+
+   // ask question timer
+   let counter = 0;
+   let id = setTimeout(function t() {
+      id = setTimeout(t,1000);
+      if (counter === 5){
+       	clearInterval(id);
+         $('.ask-question').css('display', 'block');
+      }
+      counter++;
+   })
 });
-  	$('document').ready(function () {
-
-  		// sticky
-  		$(window).scroll(function() {
-  			if( $(window).scrollTop() ) {
-  				$('.header__nav').addClass('fixed');
-  				$('.header__title').addClass('mt97');
-  			}else {
-  				$('.header__nav').removeClass('fixed');
-  				$('.header__title').removeClass('mt97');
-  			}
-  		});
-		// const navbar = $('.header__nav');
-		// const sticky = $('.services').offset().top;
-		
-		// if ($(window).scrollTop() >= sticky) {
-		// 	navbar.addClass('header__nav--sticky');
-		// }else {
-		// 	navbar.removeClass('header__nav--sticky');
-		// }
-
-
-
-  		// slide menu
-  		$('.header__menu').click(function() {
-  			$('.header-slide').toggleClass('open');
-
-  			$('.header-slide__close').click(function() {
-  				$('.header-slide').removeClass('open');
-  			});
-  		});
-
-
-  		// slider interface
-  		$('.interface__slider').slick({
-		  infinite: true,
-		  speed: 300,
-		  slidesToShow: 1,
-		  centerMode: true,
-		  variableWidth: true,
-		  arrows: false,
-		  autoplay : true,
-		  autoplaySpeed : 5000
-		});
-
-
-  		// customers
-  		let activeCuctomer = 1
-		$('.customers__slide').hide();
-		$('.customers__slide:nth-child(' + activeCuctomer + ')').fadeIn();
-
-		$('.customers__item').click(function() {
-			$('.customers__item').removeClass('customers__item--current');
-			$(this).addClass('customers__item--current');
-
-			$('.customers__slide').hide().eq( $(this).index() ).fadeIn();
-		})
-
-
-		// switch
-		$('.plan__select').click(function() {
-			$('.plan__select').removeClass('plan__select--active');
-			$(this).addClass('plan__select--active');
-		});
-	});
